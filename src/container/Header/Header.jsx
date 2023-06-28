914e06db488d336f14c27bb2270ebc4b90c4e79b
Renu Desai <renud.91@gmail.com>
2023-06-28 13:32:47 +0100

    Added Navbar to the page

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./Header.css";
+import { images } from "../../constants";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="app__header app__wrapper section__padding">Header</div>
+  );
 };
 
 export default Header;
