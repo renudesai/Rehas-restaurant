52014a99d665bc957f1bf070bdfaad4b0c1929d2
Renu Desai <renud.91@gmail.com>
2023-07-03 00:27:46 +0100

    Added special menu to the app

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -1,8 +1,30 @@
 import React from "react";
 import "./MenuItem.css";
 
-const MenuItem = () => {
-  return <div>MenuItem</div>;
+const MenuItem = ({ title, price, tags }) => {
+  return (
+    <div className="rehas__menuItem">
+      <div className="rehas__menuItem-head">
+        <div className="rehas__menutem-name">
+          <p className="p__cormorant" style={{ color: "#DCCA87" }}>
+            {title}
+          </p>
+        </div>
+
+        <div className="rehas__menuItem-dash" />
+
+        <div className="rehas__menuitem-price">
+          <p className="p__cormorant">{price}</p>
+        </div>
+      </div>
+
+      <div className="rehas__menuItem-sub">
+        <p className="p__opensans" style={{ color: "#AAA" }}>
+          {tags}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default MenuItem;
